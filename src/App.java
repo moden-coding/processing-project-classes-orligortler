65962f88d808a71086831f9445a0477774dcf07a
orligortler <orlisqueak@gmail.com>
2024-12-10 09:14:11 -0500

    progress

--- src/App.java
+++ src/App.java
@@ -1,20 +1,34 @@
-import processing.core.*;
+import java.util.ArrayList;
+import processing.core.PApplet;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+public class App extends PApplet {
+    ArrayList<Worm> worms;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
-        
-    }
-
-    public void settings(){
+    public void settings() {
         size(1100, 750);
     }
 
-    public void draw(){
-        background(0);
+    public void setup() {
+        worms = new ArrayList<>(); // Initialize worms ArrayList
+        wormMaker(); // Call wormMaker after initializing worms
+    }
 
+    public void draw() {
+        background(0);
+        for (Worm b : worms) {
+            b.display();
+            b.update();
+        }
+    }
+
+    public void wormMaker() {
+        int x = 55;
+        int y = 0;
+        Worm worm = new Worm(x, y, this);
+        worms.add(worm);
     }
 }
